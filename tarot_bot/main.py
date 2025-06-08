a851df88fb57db8fec27a44211b072542d5d29e1
fujikongu <fujikonbu0507@gmail.com>
2025-06-09 08:34:23 +0900

    Update main.py

--- tarot_bot/main.py
+++ tarot_bot/main.py
@@ -8,7 +8,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, QuickReply, QuickReplyButton, MessageAction
 
-from genre_handlers import handle_genre_selection
+from genre_handlers import handle_genre_message
 
 app = Flask(__name__)
 
